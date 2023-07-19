321fe2badd0272945d7dba33eccd4e5ae3dcc951
geekelo <efuelight12@gmail.com>
2023-07-19 10:28:03 +0100

    Implement basic struncture, display and updating todo list

---
+++ src/components/todosLogic.js
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+import TodoList from './todoList';
+import InputTodo from './todoInput';
+
+const TodosLogic = () => {
+  const [ todos, setTodos ] = useState([
+    {
+      id: 1,
+      title: 'Setup development environment',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Develop website and add content',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Deploy to live server',
+      completed: false,
+    },
+  ])
+
+  const addNewItem = (newtitle) => {
+   const newTodo = {
+      id: todos.length + 1,
+      title: newtitle,
+      completed: false,
+    };
+    
+    setTodos([...todos, newTodo])
+  }
+
+  const handleClick = (id) => {
+    setTodos((prevState) => {
+      return prevState.map((each) => {
+        if (each.id === id) {
+          return {
+            ...each,
+            completed: !each.completed
+          }
+        }
+        return each;
+      })
+    })
+  }
+
+  const delTodo = (id) => {
+    setTodos((prevState) => {
+      return prevState.filter((each) => {
+        return each.id !== id
+      }
+      )
+    })
+  }
+
+  return (
+    <div>
+      <InputTodo addNewItem={addNewItem} />
+      <TodoList todoProps={todos} setTodo={setTodos} clickFunction={handleClick} delFunction={delTodo}/>
+    </div>
+  );
+};
+
+export default TodosLogic;
